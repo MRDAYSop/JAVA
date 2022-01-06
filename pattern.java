e13185a60290b87c0b100a4a6224ef58c096f589
MRDAYSop <89299563+MRDAYSop@users.noreply.github.com>
2022-01-06 21:30:15 +0530

    Add files via upload
    This is program for making patterns in java.

---
+++ pattern.java
@@ -0,0 +1,15 @@
+class pattern
+{
+    void main(int n)
+    {
+        int i,j;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+          System.out.println();
+        }
+    }
+}
